f950783d0ccea32a46e21785cbeaae25516ffe82
Bruhathi <91585301+bruhathisp@users.noreply.github.com>
2022-06-06 17:29:35 +0530

    Add files via upload

---
+++ flask_tut/tutorial1.py
@@ -0,0 +1,26 @@
+from flask import Flask, redirect, render_template, url_for
+
+app= Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/<name>")
+def user(name):
+    return render_template("index.html", content=['1', '2','30','4','5'] )
+
+
+
+@app.route("/admin/")
+def admin():
+    return redirect(url_for("user",name="admin page"))
+
+    
+if __name__ == "__main__":
+    app.run()
+
+#create a new page
+
+
